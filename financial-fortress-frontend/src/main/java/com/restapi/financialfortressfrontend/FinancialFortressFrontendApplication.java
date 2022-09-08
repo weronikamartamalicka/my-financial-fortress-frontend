2ea4773d1149cac134fcaddda4689d789b69e533
Weronika Malicka <weronikamarta.malicka@gmail.com>
2022-09-08 14:02:57 +0200

    First implementation of home page

---
+++ financial-fortress-frontend/src/main/java/com/restapi/financialfortressfrontend/FinancialFortressFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.financialfortressfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialFortressFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialFortressFrontendApplication.class, args);
+    }
+
+}
---
+++ financial-fortress-frontend/src/main/java/com/restapi/financialfortressfrontend/HomePage.java
@@ -0,0 +1,59 @@
+package com.restapi.financialfortressfrontend;
+
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.router.Route;
+
+@Route("home")
+public class HomePage extends AppLayout {
+
+    public HomePage() {
+        DrawerToggle toggle = new DrawerToggle();
+
+        H1 title = new H1("MyFinancialFortressApp");
+        title.getStyle()
+                .set("font-size", "var(--lumo-font-size-l)")
+                .set("margin", "0");
+
+        Tab home = new Tab(
+                VaadinIcon.WALLET.create(),
+                new Span("Home")
+        );
+        Tab gold = new Tab(
+                VaadinIcon.PIGGY_BANK_COIN.create(),
+                new Span("Gold")
+        );
+        Tab bondsQuoted = new Tab(
+                VaadinIcon.DIPLOMA.create(),
+                new Span("BondsQuoted")
+        );
+
+        Tab bondsIndexed = new Tab(
+                VaadinIcon.INSTITUTION.create(),
+                new Span("BondsIndexed")
+        );
+
+        Tab emergingMarket = new Tab(
+                VaadinIcon.BUILDING_O.create(),
+                new Span("EmergingMarketETF")
+        );
+
+        Tab developedMarket = new Tab(
+                VaadinIcon.BUILDING.create(),
+                new Span("DevelopedMarketETF")
+        );
+
+        Tabs tabs = new Tabs(home, gold, bondsQuoted, bondsIndexed, emergingMarket, developedMarket);
+        tabs.setOrientation(Tabs.Orientation.VERTICAL);
+
+        addToDrawer(tabs);
+        addToNavbar(toggle, title);
+
+        setPrimarySection(Section.DRAWER);
+    }
+}
