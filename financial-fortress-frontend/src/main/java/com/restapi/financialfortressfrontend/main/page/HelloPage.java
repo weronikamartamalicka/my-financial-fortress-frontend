9cf8503bd1c19e819d618c75636d6d622cfd35e0
Weronika Malicka <weronikamarta.malicka@gmail.com>
2022-09-19 14:26:14 +0200

    Corrected hello page

--- financial-fortress-frontend/src/main/java/com/restapi/financialfortressfrontend/main/page/HelloPage.java
+++ financial-fortress-frontend/src/main/java/com/restapi/financialfortressfrontend/main/page/HelloPage.java
@@ -1,31 +1,45 @@
 package com.restapi.financialfortressfrontend.main.page;
 
-import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 @Route("")
-public class HelloPage extends VerticalLayout {
+@StyleSheet("/css/style.css")
+public class HelloPage extends HorizontalLayout {
 
     public HelloPage() {
-        add(new Text("Have you created your investment yet?"));
-        add(new Image("https://finansowaforteca.pl/wp-content/webp-express/" +
-                "webp-images/uploads/2022/09/FinForteca_mockup_3-low-res-2022-new.jpg.webp",
-                "Cannot load image"));
+
         add(new HelloPage2());
+
+        Image image = new Image("https://finansowaforteca.pl/wp-content/webp-express/" +
+                "webp-images/uploads/2022/09/FinForteca_mockup_3-low-res-2022-new.jpg.webp",
+                "Cannot load image");
+
+        add(image);
+        setHeightFull();
     }
 
-    private class HelloPage2 extends HorizontalLayout {
+    private class HelloPage2 extends VerticalLayout {
 
         public HelloPage2() {
-            Button buttonCreateInvest = new Button("No, I have not! Let's create one!");
+            add(new H1("BUILD YOUR FINANCIAL FORTRESS"));
+            add(new H3("Do you want to invest, but don't know where to start?" +
+                    " Do you feel resistance to dealing with your finances? Are you looking" +
+                    " more and more closely at your financial future at the same time knowing " +
+                    "that you need substantive support to take matters into your own hands?"));
+            add(new H4("Instead of trying to \"learn how to invest\" build a step-by-step " +
+                    "Financial Fortress with the My Financial Fortress app based on the knowledge" +
+                    " from Marcin Iwuć's bestseller, which will effectively protect your and your family's wealth!"));
+
+            Button buttonCreateInvest = new Button("WANT TO START");
             buttonCreateInvest.addClickListener(e -> {
                 buttonCreateInvest.getUI().ifPresent(ui -> ui.navigate("home2"));
             });
-            Button buttonAnalyzeInvest = new Button("Yup! I want to have a look at my portfolio");
+            Button buttonAnalyzeInvest = new Button("I HAVE MY FORTRESS");
             buttonAnalyzeInvest.addClickListener(e -> {
                 buttonAnalyzeInvest.getUI().ifPresent(ui -> ui.navigate("home"));
             });
